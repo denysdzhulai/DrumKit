67e8ea8f8f821754aa9029a2ed049cf54d439ec4
Denys <denisjuly@gmail.com>
2022-02-13 00:24:54 -0700

    Changed color palette and button style

--- index.js
+++ index.js
@@ -58,5 +58,5 @@ function buttonAnimation(currentKey) {
   activeButton.classList.add("pressed");
   setTimeout(function () {
     activeButton.classList.remove("pressed");
-  }, 100);
+  }, 150);
 }
